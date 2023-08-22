e1ba01cc9ee8d26c39d1f569f8cee046a035d9e0
ndy2 <emrdbs12@gmail.com>
2023-08-23 02:23:34 +0900

    add NOT_AUTHENTICATED

--- src/python/auth/authenticator.py
+++ src/python/auth/authenticator.py
@@ -5,6 +5,7 @@ import extra_streamlit_components as stx
 from python.auth.domain import User, UserSource
 
 Authentication = tuple[bool, Optional[User]]
+NOT_AUTHENTICATED = False, None
 
 
 class Authenticator:
@@ -17,7 +18,7 @@ class Authenticator:
     def check_cookie_and_form_authentication(self, login_form_with_input) -> Authentication:
         cookie_authenticated, cookie_authenticated_user = self.check_cookie_authentication()
 
-        form_authenticated, form_authenticated_user = False, None
+        form_authenticated, form_authenticated_user = NOT_AUTHENTICATED
         if cookie_authenticated is False:
             form_authenticated, form_authenticated_user = self._check_form_authentication(*login_form_with_input())
 
@@ -27,20 +28,17 @@ class Authenticator:
         return authenticated, user
 
     def check_cookie_authentication(self) -> Authentication:
-        return False, None
+        return NOT_AUTHENTICATED
 
     def _check_form_authentication(self, submit, username, password) -> Authentication:
-        if submit:
-            return self._check_username_password(username, password)
-        else:
-            return False, None
+        if not submit:
+            return NOT_AUTHENTICATED
+        return self._check_username_password(username, password)
 
     def _check_username_password(self, username, password) -> Authentication:
         found_user = self.user_source.load_by_username(username)
-        if found_user is None:
-            return False, None
-        elif found_user.password != password:
-            return False, None
+        if found_user is None or found_user.password != password:
+            return NOT_AUTHENTICATED
         return True, found_user
 
     def _add_cookie(self):

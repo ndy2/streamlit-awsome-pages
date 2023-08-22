f4ecccf11eabc8319f143789f614c96038c94317
ndy2 <emrdbs12@gmail.com>
2023-08-23 02:44:57 +0900

    cm

--- src/python/auth/authenticator.py
+++ src/python/auth/authenticator.py
@@ -23,7 +23,9 @@ class Authenticator:
             form_authenticated, form_authenticated_user = self._check_form_authentication(*login_form_with_input())
 
         authenticated = cookie_authenticated or form_authenticated
-        user = cookie_authenticated_user if cookie_authenticated else form_authenticated_user if form_authenticated else None
+        user = cookie_authenticated_user if cookie_authenticated \
+            else form_authenticated_user if form_authenticated \
+            else None
 
         return authenticated, user
 
